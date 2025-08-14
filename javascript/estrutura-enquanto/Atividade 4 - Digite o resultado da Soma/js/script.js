a4e89646116eb867f3129718a34c9e2db547a1a7
Hiron <hiron_novais@hotmail.com>
2025-08-13 21:33:34 -0300

    Alterações do dia 13/08/2025

--- javascript/estrutura-enquanto/Atividade 4 - Digite o resultado da Soma/js/script.js
+++ javascript/estrutura-enquanto/Atividade 4 - Digite o resultado da Soma/js/script.js
@@ -1,8 +1,8 @@
-function procDados(){
+function calcIMC(){
     let soma=0
     console.clear()
-    v1 = parseInt(document.getElementById("inptVal1").value)
-    v2 = parseInt(document.getElementById("inptVal2").value)
+    v1 = parseInt(document.getElementById("inptPeso").value)
+    v2 = parseInt(document.getElementById("inptAltura").value)
 
     while(soma!=(v1+v2)){
         soma = parseInt(prompt("Digite o valor da soma dos números informados: "))
--- javascript/estrutura-enquanto/Atividade 5 - Calcula IMC/js/script.js
+++ javascript/estrutura-enquanto/Atividade 5 - Calcula IMC/js/script.js
@@ -1,16 +1,18 @@
-function procDados(){
-    let soma=0
+function calculaIMC(){
+    let imc=0
+    let continuar=true
     console.clear()
-    v1 = parseInt(document.getElementById("inptVal1").value)
-    v2 = parseInt(document.getElementById("inptVal2").value)
+    //v1 = parseInt(document.getElementById("inptVal1").value)
+    //v2 = parseInt(document.getElementById("inptVal2").value)
 
-    while(soma!=(v1+v2)){
-        soma = parseInt(prompt("Digite o valor da soma dos números informados: "))
-        if(soma!=(v1+v2)){
-            console.log("Tente Novamente!")
-        }else{
-            console.log("Resultado Correto!")
-        }
+    while(continuar){
+        console.log("_____________________________________")
+        peso = parseFloat(prompt("Digite o seu peso: "))
+        altura = parseFloat(prompt("Digite a sua altura: "))
+        imc = peso / (altura * altura)
+        console.log("Seu IMC é: "+imc+"!")
+        continuar = window.confirm("Deseja realizar outro calculo de IMC?")
+        //if(prompt("Deseja fazer outro calculo? Digite 1 para sim 0 para não: ")==0){ continuar = false }
     }
 
     return false;
