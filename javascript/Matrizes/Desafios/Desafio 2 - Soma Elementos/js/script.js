695d2f5cbaa5154c9d22cdc23d42a20300e2e148
Hiron <hiron_novais@hotmail.com>
2025-08-20 21:27:09 -0300

    Finalizado Desafio Matrizes

--- javascript/Matrizes/Desafios/Desafio 2 - Soma Elementos/js/script.js
+++ javascript/Matrizes/Desafios/Desafio 2 - Soma Elementos/js/script.js
@@ -1,28 +1,26 @@
 function procFormulario(){
+    /*Desafio2
+
+    A empresa “Só soma Ltda.” necessita de uma solução que leia uma matriz 4x3 de 
+    números reais e mostre a soma de seus elementos. */
     console.clear()
     let matriz = []
     let soma = 0, linha = 0
     
-    for(let i=0; i < 2; i++){
+    for(let i=0; i < 4; i++){
         matriz[i] = []
         
-        for(let c=0; c<2; c++){
+        for(let c=0; c<3; c++){
             
             matriz[i][c] = parseInt(prompt("Digite um valor para armazenar: "))
         }
     }
-    do{
-        linha = parseInt(prompt("Escolha uma linha para somar os valores: "))
-        if(linha > 1){
-            alert("Linha inválida, a matriz não possui a linha: "+linha)
+    
+    for(let i=0; i < 4; i++){        
+        for(let c=0; c<3; c++){
+            soma += matriz[i][c]
         }
-    }while(linha >1)
-
-    for(let c=0; c<2; c++){
-        soma += matriz[linha][c]
     }
-    console.log("Valor da matriz")
-    console.log(matriz)
-    console.log("Valor da soma da linha: "+linha+" é igual a: "+soma)
+    console.log("Soma dos valores: "+soma)
     return false;
 }
--- javascript/Matrizes/Desafios/Desafio 4 - Soma Linha, Soma tudo/js/script.js
+++ javascript/Matrizes/Desafios/Desafio 4 - Soma Linha, Soma tudo/js/script.js
@@ -1,11 +1,29 @@
 function procFormulario(){
-    let matriz = []
-    for(let i=0; i < 2; i++){
+    /* Desafio 4 
+
+    O professor “Ludovico Volpato” necessita de uma solução em que dada uma matriz 
+    de 4x5 elementos, fazer um algoritmo para calcular a soma de cada linha e a soma 
+    de todos os elementos.*/
+
+    console.clear()
+    let matriz = [], somaLinha = [], total=0
+    
+    for(let i=0; i < 4; i++){
         matriz[i] = []
-        for(let c=0; c<3; c++){ 
-            matriz[i][c] = parseInt(prompt("Digite ai um valor: "))
+        somaLinha[i] = 0
+        for(let c=0; c<5; c++){
+            matriz[i][c] = parseInt(prompt("Digite um valor para a linha: "+(i+1)+" Coluna: "+(c+1)+": "))
+            somaLinha[i] += parseInt(matriz[i][c])
+            total += parseInt(matriz[i][c])
         }
     }
+    console.log("Matriz")
     console.log(matriz)
-    return false;
+    
+    console.log("Soma de cada linha")
+    console.log(somaLinha)
+
+    console.log("Total da matriz: "+total)
+
+    return false
 }
