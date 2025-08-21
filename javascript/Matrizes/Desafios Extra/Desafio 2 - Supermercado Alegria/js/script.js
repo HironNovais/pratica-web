3b914eccbed481f1d11043abc9fa1128d1e32ae2
Hiron <hiron_novais@hotmail.com>
2025-08-21 20:18:09 -0300

    Desafio Extra

--- javascript/Matrizes/Desafios Extra/Desafio 2 - Supermercado Alegria/js/script.js
+++ javascript/Matrizes/Desafios Extra/Desafio 2 - Supermercado Alegria/js/script.js
@@ -1,26 +1,42 @@
-function procFormulario(){
-    /*Desafio2
 
-    A empresa “Só soma Ltda.” necessita de uma solução que leia uma matriz 4x3 de 
-    números reais e mostre a soma de seus elementos. */
+
+function enviaDados(event){
+    event.preventDefault();
+    /*Super mercdao. */
     console.clear()
-    let matriz = []
-    let soma = 0, linha = 0
+    let valores = [], clientes = []
+    let qtd = 0, linha = 0
     
-    for(let i=0; i < 4; i++){
-        matriz[i] = []
-        
+    for(let i=0; i < 3; i++){
+        valores[i] = []
+        clientes[i] = prompt("Digite o nome do cliente: ")
         for(let c=0; c<3; c++){
-            
-            matriz[i][c] = parseInt(prompt("Digite um valor para armazenar: "))
+            valores[i][c] = parseInt(prompt("Digite o valor da compra do mês "+(c+1)+": "))
         }
     }
-    
-    for(let i=0; i < 4; i++){        
-        for(let c=0; c<3; c++){
-            soma += matriz[i][c]
-        }
+    localStorage.setItem("valores", JSON.stringify(valores));
+    localStorage.setItem("clientes", JSON.stringify(clientes));
+
+    return false;
+}
+
+function listaCompras(event){
+    event.preventDefault();
+    let valores = JSON.parse(localStorage.getItem("valores")) || [];
+    let clientes = JSON.parse(localStorage.getItem("clientes")) || [];
+    let maiorNome = Math.max(...clientes.map(e => e.length));
+    for(let l=0; l<3; l++){
+        clientes[l] = clientes[l]+' '.repeat(maiorNome - clientes[l].length);
     }
-    console.log("Soma dos valores: "+soma)
+    console.log("Valores Comprados")
+    let impressao
+    for(let i=0; i<valores.length; i++){
+        impressao = clientes[i]
+        for(let c=0; c<3; c++){
+            impressao +=" | Mês 0"+(c+1)+": "+valores[i][c]
+        }
+        console.log(impressao)
+    }
+    
     return false;
 }
