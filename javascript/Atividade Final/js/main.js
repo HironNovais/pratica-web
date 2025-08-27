294a453b254f9212211f547bb526604c501667d5
Hiron <hiron_novais@hotmail.com>
2025-08-26 21:40:01 -0300

    Alterações trabalho Final

--- javascript/Atividade Final/js/main.js
+++ javascript/Atividade Final/js/main.js
@@ -1,6 +1,13 @@
 $( document ).ready(function() {
-    $("#button").click( function(){
-        alert('button clicked');
-    })
-});
+    /*$("#estruturas_condicionais_atv_1").click( function(){
 
+    })*/
+    $(".btn-acao").click( function(){
+
+        let btn_clicado = $(this).attr("id");
+        if(btn_clicado == 'estruturas_condicionais_if_atv_1'){
+            //alert($('table tr:nth-child(1) td:nth-child(2)').text());
+            
+        } 
+    });
+});
--- javascript/estrutura-enquanto/exemplo/js/valproc.js
+++ javascript/estrutura-enquanto/exemplo/js/valproc.js
@@ -1,9 +1,9 @@
 function procFormulario(){
     let cont = 1
     while(cont <11){
-        //if(cont%2==0){
+        if(cont%2==0){
             console.log("Valor do contador: "+cont)
-        //}
+        }
         cont++
     }
     return false;
