07e10d1a3098b0e3b48f98db7c780a6b911e21ab
Hiron <hiron_novais@hotmail.com>
2025-09-02 20:31:06 -0300

    Comitado

--- javascript/AtividadeFinal/js/main.js
+++ javascript/AtividadeFinal/js/main.js
@@ -2,19 +2,20 @@ $( document ).ready(function() {
     
     $(".btn-acao").click( function(event){
         let link_completo=""
-        let btn_clicado = $(this).attr("id"); // Pegar o btn clicado
+        let btn_clicado = $(this).attr("id");
 
         let row = $(this).closest("tr");
         let nomeAtividade = row.find("td").eq(0).text().trim();
         let descricao = row.find("td").eq(1).text().trim();
-        let codigo = row.find("td").eq(3).text().trim();
+        //let codigo = row.find("td").eq(3).text().trim();
 
         link_completo = "./"+btn_clicado+".html"
-        location.replace(link_completo)
 
         localStorage.setItem("nomeAtividade", nomeAtividade);
         localStorage.setItem("descricaoAtividade", descricao);
-        localStorage.setItem("codigo", codigo)
-          
+        localStorage.setItem("link", link_completo)
+
+        window.open(link_completo, '_self')
+        //location.replace(link_completo)
     });
 });
--- javascript/AtividadeFinal/js/visualiza.js
+++ javascript/AtividadeFinal/js/visualiza.js
@@ -1,5 +1,13 @@
 $( document ).ready(function() {
-    let opcao, codigoCompleto
     document.getElementById("nomeAtividade").innerText = localStorage.getItem("nomeAtividade");
     document.getElementById("descricaoAtividade").innerText = localStorage.getItem("descricaoAtividade");
+    //document.getElementById("link").innerText = localStorage.getItem("link");    
 })
+
+function voltar(){
+    window.open('index.html', '_self')
+}
+
+function voltarCodigo(){
+    window.open(localStorage.getItem("link"), '_self')
+}
